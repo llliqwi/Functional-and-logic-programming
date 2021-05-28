f628b33cc7875dd1bb23b6b1747a976002c93197
liqwi <68617430+llliqwi@users.noreply.github.com>
2021-05-28 23:51:10 +0300

    Update 14.1.kt

--- Lab14/14.1.kt
+++ Lab14/14.1.kt
@@ -1,9 +1,6 @@
-fun upsum(y: Int): Int = if (y == 0) y else upsum(y/10)+y%10
-
-fun main(args:Array<String>)
-{
-    println("Введите число ->")
-    val x = readLine()!!.toInt()
-    print("Сумма цифр: " )
-    println(upsum(x))
+fun sumUp(num: Int): Int = if (num == 0) num else sumUp(num/10)+num%10
+fun main(args:Array<String>) {
+     print("Введите число ->")
+     val x = readLine()!!.toInt()
+     println("Сумма цифр числа: ${sumUp(x)}")
 }
